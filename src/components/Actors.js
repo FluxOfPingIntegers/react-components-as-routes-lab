3505334974c0334b8c9f4ccb7e93899a55244d77
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-10-19 13:14:11 -0400

    commit tree d617506f731fe13a36aef15f7ecb1b09aa817389

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { actors } from '../data';
 
+const actorCard = ( {name, movies} ) => {
+  return (
+    <div className="actorCard">
+      <h2>Name: {name}</h2>
+      <h3>Movies:</h3>
+      <ul>
+        {movies.map( (movie) => {return <li>{movie}</li>})}
+      </ul>
+    </div>
+  )
+}
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => actorCard(actor))}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { movies } from '../data';
 
+const movieCard = ( {title, time, genres} ) => {
+  return (
+    <div className="movieCard">
+      <h2>Title: {title}</h2>
+      <h3>Time: {time}</h3>
+      <p>Genres:</p>
+      <ul>
+        {genres.map( (genre) => {return <li>{genre}</li>})}
+      </ul>
+    </div>
+  )
+}
+
 const Movies = () => {
   return (
-    <div>
-        {/*{code here}*/}
+    <div className="Movies">
+      <h1>Movies Page</h1>
+      {movies.map(movie => movieCard(movie))}
     </div>
   );
 };
